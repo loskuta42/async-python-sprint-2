9841f3c0fa9a48cdcaac3fbc5f3e174c2ce1c9d8
Alexey Lobarev <loskuta42@yandex.ru>
2022-10-12 08:44:39 +0300

    start project

--- job.py
+++ job.py
@@ -1,9 +1,28 @@
+from datetime import datetime
+from typing import Callable
+from threading import Timer, Condition
+
+
 class Job:
-    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
-        pass
+    def __init__(
+            self,
+            target: Callable,
+            *args,
+            start_at: str = "",
+            max_working_time: int = -1,
+            tries: int = 0,
+            dependencies: list = []
+    ):
+        self.start_at = datetime.strptime(start_at, '%H:%M').time()
+        self.max_working_time = max_working_time
+        self.tries = tries
+        self.dependencies = dependencies
+        self.target = target
+        self.args = args
 
     def run(self):
-        pass
+        if self.start_at:
+            t = Timer(self.run, )
 
     def pause(self):
         pass
